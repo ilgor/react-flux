17ff435ca340c09dfe042658d08d1b5b32876d13
ilgor <ilgorbek@gmail.com>
2018-08-15 14:47:24 -0500

    + Added bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,23 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');  // Run local webserver
 var open = require('gulp-open');        // Open a URL in web browser
-
+var browserify = require('browserify'); // bundle JS
+var reactify = require('reactify');     // Transform React JSX to JS    
+var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
+var concat = require('gulp-concat');    // Concat Files
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+        ],
+        dist: './dist',
+        mainJs: './src/main.js',
     }
 }
 
@@ -32,8 +41,25 @@ gulp.task('html', function() {
     gulp.src(config.paths.html).pipe(gulp.dest(config.paths.dist)).pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-    gulp.watch(config.paths.html, ['html']);
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+    .transform(reactify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(config.paths.dist + '/scripts'))
+    .pipe(connect.reload());
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.html, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);

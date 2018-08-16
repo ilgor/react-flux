0b72c8f0cefc3b4cbd6b2668d9cf15b4d0f59cba
ilgor <ilgorbek@gmail.com>
2018-08-15 19:29:59 -0500

    + Added Lint and React

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify'); // bundle JS
 var reactify = require('reactify');     // Transform React JSX to JS    
 var source = require('vinyl-source-stream'); // Use conventional text streams with Gulp
 var concat = require('gulp-concat');    // Concat Files
+var lint = require('gulp-eslint');
 
 var config = {
     port: 9005,
@@ -57,9 +58,15 @@ gulp.task('css', function() {
         .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.html, ['js']);
+gulp.task('lint', function() {
+    return gulp.src(config.paths.js)
+        .pipe(lint({ configFile: 'eslint.config.json' }))
+        .pipe(lint.format());
 });
 
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']);
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
